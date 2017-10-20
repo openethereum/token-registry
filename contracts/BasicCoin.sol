3fa0eb953e99d12c182d0c303b39db6863fa15ab
keorn <pczaban@gmail.com>
2017-10-20 15:22:43 +0100

    token.sol

--- contracts/BasicCoin.sol
+++ contracts/BasicCoin.sol
@@ -5,18 +5,7 @@
 pragma solidity ^0.4.1;
 
 import "Owned.sol";
-
-// ECR20 standard token interface
-contract Token {
-	event Transfer(address indexed from, address indexed to, uint256 value);
-	event Approval(address indexed owner, address indexed spender, uint256 value);
-
-	function balanceOf(address _owner) constant returns (uint256 balance);
-	function transfer(address _to, uint256 _value) returns (bool success);
-	function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-	function approve(address _spender, uint256 _value) returns (bool success);
-	function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-}
+import "Token.sol";
 
 // TokenReg interface
 contract TokenReg {
---
+++ contracts/Token.sol
@@ -0,0 +1,14 @@
+//! ECR20 standard token interface
+
+pragma solidity ^0.4.17;
+
+contract Token {
+	event Transfer(address indexed from, address indexed to, uint256 value);
+	event Approval(address indexed owner, address indexed spender, uint256 value);
+
+	function balanceOf(address _owner) constant public returns (uint256 balance);
+	function transfer(address _to, uint256 _value) public returns (bool success);
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+	function approve(address _spender, uint256 _value) public returns (bool success);
+	function allowance(address _owner, address _spender) constant public returns (uint256 remaining);
+}
