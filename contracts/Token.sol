1b8361769e54dd34aea120f0ad1d90856b8eeae7
Robert Habermeier <rphmeier@gmail.com>
2017-11-17 15:03:10 +0100

    a few more refactors according to review

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.4.17;
 
-contract Token {
+interface Token {
 	event Transfer(address indexed from, address indexed to, uint256 value);
 	event Approval(address indexed owner, address indexed spender, uint256 value);
 
