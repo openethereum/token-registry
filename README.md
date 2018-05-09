8364420957ece2e0aadf74d0277dd85443afd8bb
Wei Tang <hi@that.world>
2018-05-09 19:40:02 +0800

    Initial commmit

---
+++ README.md
@@ -0,0 +1,9 @@
+# token-registry
+
+[![Build Status][travis-image]][travis-url]
+[![Solidity Coverage Status][coveralls-image]][coveralls-url]
+
+[travis-image]: https://travis-ci.org/parity-contracts/token-registry.svg?branch=master
+[travis-url]: https://travis-ci.org/parity-contracts/token-registry
+[coveralls-image]: https://coveralls.io/repos/github/parity-contracts/token-registry/badge.svg?branch=master
+[coveralls-url]: https://coveralls.io/github/parity-contracts/token-registry?branch=master
---
+++ contracts/Token.sol
@@ -0,0 +1,28 @@
+//! The ECR20 standard-token interface.
+//!
+//! Copyright 2016 Peter Czaban, Parity Technologies Ltd.
+//!
+//! Licensed under the Apache License, Version 2.0 (the "License");
+//! you may not use this file except in compliance with the License.
+//! You may obtain a copy of the License at
+//!
+//!     http://www.apache.org/licenses/LICENSE-2.0
+//!
+//! Unless required by applicable law or agreed to in writing, software
+//! distributed under the License is distributed on an "AS IS" BASIS,
+//! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//! See the License for the specific language governing permissions and
+//! limitations under the License.
+
+pragma solidity ^0.4.17;
+
+interface Token {
+	event Transfer(address indexed from, address indexed to, uint256 value);
+	event Approval(address indexed owner, address indexed spender, uint256 value);
+
+	function balanceOf(address _owner) constant public returns (uint256 balance);
+	function transfer(address _to, uint256 _value) public returns (bool success);
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+	function approve(address _spender, uint256 _value) public returns (bool success);
+	function allowance(address _owner, address _spender) constant public returns (uint256 remaining);
+}
