b81955ef572387de9959f3f8e813027ef5445fca
André Silva <andre.beat@gmail.com>
2018-06-01 16:29:29 +0100

    fix code style

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -18,17 +18,20 @@ pragma solidity ^0.4.17;
 
 
 contract Owned {
+	event NewOwner(address indexed old, address indexed current);
+
+	address public owner = msg.sender;
+
 	modifier onlyOwner {
 		require(msg.sender == owner);
 		_;
 	}
 
-	event NewOwner(address indexed old, address indexed current);
-
-	function setOwner(address _new) onlyOwner public {
+	function setOwner(address _new)
+		external
+		onlyOwner
+	{
 		emit NewOwner(owner, _new);
 		owner = _new;
 	}
-
-	address public owner = msg.sender;
 }
